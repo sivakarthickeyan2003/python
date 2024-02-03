b872f16683d0959167490b13682a6839e65b93e7
sivakarthickeyan2003 <129270959+sivakarthickeyan2003@users.noreply.github.com>
2024-02-03 14:29:48 +0530

    Add files via upload

---
+++ server (2).py
@@ -0,0 +1,29 @@
+import socket
+def server_program():
+    # get the hostname
+    host = socket.gethostname()
+    #host = "192.168.0.17" 
+    
+    port = 5000  # initiate port no above 1024
+
+    server_socket = socket.socket()  #Creating the socket 
+    # look closely. The bind() function takes tuple as argument
+    server_socket.bind((host, port))  # bind host address and port together
+
+    # configure how many client the server can listen simultaneously
+    server_socket.listen(2)
+    conn, address = server_socket.accept()  # accept new connection
+    print("Connection from: " + str(address))
+    while True:
+        # receive data stream. it won't accept data packet greater than 1024 bytes
+        data = conn.recv(1024).decode()
+        if not data:
+            # if data is not received break
+            break
+        print("from connected user: " + str(data))
+        data = input(' -> ')
+        conn.send(data.encode())  # send data to the client
+
+    conn.close()  # close the connection
+server_program()
+
